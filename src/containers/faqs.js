88c82c8307745b4fb668ac65bc7e1554c82d75db
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-09-30 21:24:48 -0300

    Minor improvements

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -8,9 +8,9 @@ export function FaqsContainer() {
       <Accordion.Title>Frequently Asked Questions</Accordion.Title>
       <Accordion.Frame>
         {faqsData.map((item) => (
-          <Accordion.Item key={item.id}>
-            <Accordion.Header>{item.header}</Accordion.Header>
-            <Accordion.Body>{item.body}</Accordion.Body>
+          <Accordion.Item key={ item.id }>
+            <Accordion.Header>{ item.header }</Accordion.Header>
+            <Accordion.Body>{ item.body }</Accordion.Body>
           </Accordion.Item>
         ))}
       </Accordion.Frame>
