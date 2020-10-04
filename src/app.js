dea56242fdc5a48dde7bb8ef852cb6e610e95860
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-04 18:08:49 -0300

    Fixed exports

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { useAuthListener } from './hooks'
 import * as ROUTES from './constants/routes'
-import { Home, Browse, SignIn, SignUp } from './pages'
+import { Home, Browse, Signin, Signup } from './pages'
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
-export function App() {
+export default function App() {
   const { user } = useAuthListener()
 
   return (
     <Router>
       <Switch>
         <IsUserRedirect user={ user } loggedInPath={ ROUTES.BROWSE } path={ ROUTES.SIGN_IN }>
-          <SignIn />
+          <Signin />
         </IsUserRedirect>
         <IsUserRedirect user={ user } loggedInPath={ ROUTES.BROWSE } path={ ROUTES.SIGN_UP }>
-          <SignUp />
+          <Signup />
         </IsUserRedirect>
         <ProtectedRoute user={ user } path={ ROUTES.BROWSE }>
           <Browse />
